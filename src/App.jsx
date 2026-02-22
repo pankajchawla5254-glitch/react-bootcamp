4eb1941d496f42d520466122b09d037283b263fe
Pankaj Kumar <pankajchawla5254@gmail.com>
2026-02-23 00:20:12 +0530

    basic ui styling

--- src/App.jsx
+++ src/App.jsx
@@ -27,13 +27,7 @@ function App() {
   };
 
   return (
-    <div style={{
-      maxWidth: "400px",
-      margin: "50px auto",
-      padding: "20px",
-      border: "1px solid #ddd",
-      borderRadius: "8px"
-    }}>
+    <div className="app">
       <h1>Todo App</h1>
 
       <input
