d52d0faf846f578220b20e759559bacc95a6ef2c
Pankaj Kumar <pankajchawla5254@gmail.com>
2026-02-22 18:35:18 +0530

    day1 react setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Profile from "./components/Profile";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,10 @@
+function Profile() {
+  return (
+    <div>
+      <h2>Pankaj</h2>
+      <p>React Intern</p>
+    </div>
+  );
+}
+
+export default Profile;
