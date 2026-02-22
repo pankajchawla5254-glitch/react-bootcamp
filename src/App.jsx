3e0390c469e5c28e58f25ecd46d64c1542de7a17
Pankaj Kumar <pankajchawla5254@gmail.com>
2026-02-22 18:50:21 +0530

    day2 props and reusable components

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ function App() {
   return (
     <div>
       <h1>Hello React</h1>
-      <Profile />
+
+      <Profile name="Pankaj Kumar" role="React Intern" location="India" />
+
+      <Profile name="John Doe" role="Frontend Developer" location="USA" />
     </div>
   );
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,9 @@
-function Profile() {
+function Profile(props) {
   return (
-    <div>
-      <h2>Pankaj</h2>
-      <p>React Intern</p>
+    <div style={{ border: "1px solid gray", padding: "12px", marginTop: "10px" }}>
+      <h2>{props.name}</h2>
+      <p>{props.role}</p>
+      <small>{props.location}</small>
     </div>
   );
 }
