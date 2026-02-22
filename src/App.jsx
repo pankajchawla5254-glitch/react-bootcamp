da2d79d9f1c66aa0535a2a82bda4346ed40df2b5
Pankaj Kumar <pankajchawla5254@gmail.com>
2026-02-22 19:02:54 +0530

    day3 useState counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import Profile from "./components/Profile";
 
 function App() {
+  const [count, setCount] = useState(0)
+  
   return (
     <div>
       <h1>Hello React</h1>
+
+      <h2>Count: {count}</h2>
+
+      <button onClick={()=> setCount(count+1)}>
+        Increase
+      </button>
 
       <Profile name="Pankaj Kumar" role="React Intern" location="India" />
 
